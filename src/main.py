d914637c8b03713ab3f9829f676c38e1f7ed8c92
TeaWhyDee <daniel.555365@gmail.com>
2023-10-05 01:25:20 +0300

    tunic constants wip

--- src/main.py
+++ src/main.py
@@ -1,5 +1,21 @@
-import eng_to_ipa as p
-  
-  
-result = p.convert("Hello Geeks")
-print(result)
+import sys
+from phonemizer import phonemize
+from phonemizer.separator import Separator
+
+from tunic_constants import phoneme_to_enum, TUNIC_PHONEMES
+
+text = sys.argv[1:]
+words = " ".join(text).split(" ")
+
+phonemes = phonemize(
+    words,
+    language='en-us',
+    separator=Separator(phone='|', word='='),
+    preserve_punctuation=True,
+    njobs=4)
+
+# print(phonemes)
+
+for word in phonemes:
+    print(word.split("|")[0:-1])
+
